345a096e32bf24397b94dd09bc8ae9c3c4dc1def
mmolina <mlm15r@my.fsu.edu>
2019-10-04 17:53:56 -0400

    Creating Jet and skateboard class
    Finished with Jet class
    
    Finish with Skateboard class
    
    Created the last class, Unicycle

---
+++ Unicycle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mmolina on 10/4/2019.
+//
+
+
---
+++ Unicycle.h
@@ -0,0 +1,8 @@
+//
+// Created by mmolina on 10/4/2019.
+//
+
+#ifndef ASSIGNMENT2_UNICYCLE_H
+#define ASSIGNMENT2_UNICYCLE_H
+
+#endif //ASSIGNMENT2_UNICYCLE_H
